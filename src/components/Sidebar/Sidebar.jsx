eac2fe997e7c8094b27c650fc578de4fcabd0d9b
Genesis Vivas <genesis.vivasb@gmail.com>
2023-03-31 13:31:29 -0700

    Add images and fix nav-bar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,46 +1,56 @@
 import "../Sidebar/Sidebar.scss";
-import { Link } from "react-router-dom";
+import logo from "../../assets/images/circle.svg";
+import homeIcon from "../../assets/images/Home-white.svg";
+import searchIcon from "../../assets/images/Search-last.svg";
+import bookIcon from "../../assets/images/Book-white.svg";
 
 const Sidebar = () => {
-
-
-    return (
-
-        <>
-
-            <aside className="sideNav">
-                <nav className="sideNav-nav">
-                    <ul className="sideNav-nav__list">
-                        <li className="sideNav-nav__item">
-                            <a href="#" > 
-                                <img className="sideNav-nav__item-image" src="" alt=""/> 
-                            </a>
-                        </li>
-                        <li className="sideNav-nav__item">
-                        <a to="#" > 
-                                <img className="sideNav-nav__item-image" src="" alt=""/> 
-                        </a>
-                        </li>
-                        <li className="sideNav-nav__item">
-                        <a href="#" > 
-                                <img className="sideNav-nav__item-image" src="" alt=""/> 
-                         
-                        </a>
-                        </li>
-                        <li className="sideNav-nav__item">
-                        <a to="#" > 
-                                <img className="sideNav-nav__item-image" src="" alt=""/> 
-                        </a>
-                        </li>
-                    </ul>
-                </nav>
-            </aside>
-
-        </>
-
-    );
-
-
-}
+  return (
+    <>
+      <aside className="sideNav">
+        <nav className="sideNav-nav">
+          <ul className="sideNav-nav__list">
+            <li className="sideNav-nav__item">
+              <a href="#">
+                <img
+                  className="sideNav-nav__item-image"
+                  src={logo}
+                  alt="logo"
+                />
+              </a>
+            </li>
+            <li className="sideNav-nav__item">
+              <a href="#">
+                <img
+                  className="sideNav-nav__item-image"
+                  src={homeIcon}
+                  alt="home icon"
+                />
+              </a>
+            </li>
+            <li className="sideNav-nav__item">
+              <a to="#">
+                <img
+                  className="sideNav-nav__item-image"
+                  src={searchIcon}
+                  alt="search icon"
+                />
+              </a>
+            </li>
+            <li className="sideNav-nav__item">
+              <a href="#">
+                <img
+                  className="sideNav-nav__item-image"
+                  src={bookIcon}
+                  alt="book icon"
+                />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+    </>
+  );
+};
 
 export default Sidebar;
