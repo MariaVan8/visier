dbbc0ae19039b95b5b8f4123121a9a1d190456e7
kabdulka <kenabdulka@gmail.com>
2023-03-31 12:04:39 -0700

    fix merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Content from "./components/Content/Content";
-import Sidebar from "./components/Sidebar/Sidebar";
+// import resignByFamily from "./assets/images/resignation-family.png";
 
 function App() {
   return (
-
     <>
-      
       <Header />
-      <Sidebar />
-      <Content>
-        <h2 className="content__title">
+      <Content />
+      {/* <h2 className="content__title">
           What is the resignation rate for different job families?
         </h2>
         <h3 className="content__description">
-          Job family with highest resignation rate is Individual Contributor 2
-          at 18.9%.
+          Job family with the highest resignation rate is Individual Contributor
+          2 at 18.9%.
+        </h3> */}
+      {/* </Content> */}
+      {/* <Content>
+        <h2 className="content__title">
+          What is the resignation rate at different performance levels?
+        </h2>
+        <h3 className="content__description">
+          Performance level with the highest resignation rate is Low Performer
+          at 18.4%.
         </h3>
-      </Content>
-      <Content />
+      </Content> */}
       <Content />
       <Content />
     </>
