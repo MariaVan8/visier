0f27228905f91e52e85955449d17253062570c94
kabdulka <kenabdulka@gmail.com>
2023-03-31 12:18:28 -0700

    another merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,5 @@
 import "./App.scss";
-import Header from "./components/Header/Header";
-import Content from "./components/Content/Content";
 import Sidebar from "./components/Sidebar/Sidebar";
-
-function App() {
-  return (
-
-    <>
-      
-      <Header />
-      <Sidebar />
-      <Content>
-        <h2 className="content__title">
-          What is the resignation rate for different job families?
-        </h2>
-        <h3 className="content__description">
-          Job family with highest resignation rate is Individual Contributor 2
-          at 18.9%.
-        </h3>
-      </Content>
-      <Content />
-      <Content />
-      <Content />
-    </>
-  );
-}
-
-export default App;
-import "./App.scss";
 import Header from "./components/Header/Header";
 import Content from "./components/Content/Content";
 // import resignByFamily from "./assets/images/resignation-family.png";
@@ -35,7 +7,9 @@ import Content from "./components/Content/Content";
 function App() {
   return (
     <>
+         
       <Header />
+      <Sidebar />
       <Content />
       {/* <h2 className="content__title">
           What is the resignation rate for different job families?
