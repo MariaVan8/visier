3a8478d21d90eb37bec6da562bf0dc85bac2209f
Genesis Vivas <genesis.vivasb@gmail.com>
2023-03-31 09:36:39 -0700

    Add Content component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import Content from "./components/Content/Content";
 
 function App() {
   return (
     <>
       <Header />
+      <Content>
+        <h2 className="content__title">
+          What is the resignation rate for different job families?
+        </h2>
+      </Content>
+      <Content />
+      <Content />
+      <Content />
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,20 +4,20 @@ function Header() {
   return (
     <div className="header">
       <h1 className="header__title">Who is leaving?</h1>
-      <h3 className="header__subtitle">
+      <h4 className="header__subtitle">
         Identify resignations for different for talent groups.
-      </h3>
+      </h4>
       <div className="header__tags">
         <a className="header__tag" href="#">
           Feb 2022
         </a>
         <a className="header__tag header__tag--color" href="#">
-          Add a filter
+          + Add a filter
         </a>
         <a className="header__tag" href="#">
           ..
         </a>
-        <a className="header__tag" href="#">
+        <a className="header__tag header__tag--dots" href="#">
           ...
         </a>
       </div>
